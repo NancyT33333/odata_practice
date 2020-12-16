5a636208b2b179248ca4604fbe36129486001d20
Anna Tuzova <nancyt33333@gmail.com>
2020-12-16 10:48:38 +0300

    full name mapped to db column Firstname in non writable mode so full name is not persistent (transient)

--- src/ru/teamidea/odatapractice/products/models/Order.java
+++ src/ru/teamidea/odatapractice/products/models/Order.java
@@ -7,16 +7,19 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.NamedNativeQueries;
+import javax.persistence.NamedNativeQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
 import org.hibernate.validator.constraints.NotBlank;
 
 
 
 @Entity
 @Table(name = "ORDERS")
-
+@NamedNativeQueries({
+   @NamedNativeQuery(name = "Order.ReadAllQuery", query = "SELECT * FROM Order o")
+})
 public class Order {
     
     @Id
@@ -41,12 +44,16 @@ public class Order {
         this.orderItems = new ArrayList<OrderItem>();
     }
     
+   
+    
     @Column(name = "SURNAME")
     private String surName;
     
     public String getSurName() {
         return this.surName;
     }
+    
+   
 
     public void setSurName(String surName) {
         this.surName = surName;
@@ -97,20 +104,16 @@ public class Order {
         this.email = email;
     }
         
-//    @Column(name="(select SURNAME, FIRSTNAME from ORDERS) as fullName",insertable = false, updatable = false)
-//    public String getFullName() {
-//        return fullName;
-//    }
-//    
-// 
-//    transient private String fullName;
-//    
-//    public String getFullName() {
-//        return this.firstName + this.surName;
-//    }
-//
-//    public void setFullName(String fullName) {
-//        this.fullName = fullName;
-//    }   
+
+    @Column(name="FIRSTNAME", updatable=false, insertable=false)
+    private String fullName;
+    
+    public String getFullName() {
+        return this.firstName + this.surName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }   
 
 }
